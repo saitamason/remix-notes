9dc711ffffb710f21a8af0160ee57af07c76ee86
dawidlehai <haidawidle@gmail.com>
2023-01-08 18:43:28 +0100

    Added server-side code to save notes into json file

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +8,17 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+export async function action({ request }: { request: Request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
 }
 
 export function links() {
