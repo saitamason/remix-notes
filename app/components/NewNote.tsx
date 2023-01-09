f266b3c5a89e99d42f6a29116c370cd7967c9974
dawidlehai <haidawidle@gmail.com>
2023-01-09 14:02:50 +0100

    Changed form to Form, so that the page won't reload after adding new note

--- app/components/NewNote.tsx
+++ app/components/NewNote.tsx
@@ -1,8 +1,9 @@
 import styles from "./NewNote.css";
+import { Form } from "@remix-run/react";
 
 export default function NewNote() {
   return (
-    <form method="post" id="note-form">
+    <Form method="post" id="note-form">
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
@@ -14,7 +15,7 @@ export default function NewNote() {
       <div className="form-actions">
         <button>Add Note</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
