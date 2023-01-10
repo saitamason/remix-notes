0ef003a73a7aada041d4ade613cc40deb24bd98f
dawidlehai <haidawidle@gmail.com>
2023-01-10 12:03:06 +0100

    Created note details page with dynamic routing

--- app/components/NoteList.tsx
+++ app/components/NoteList.tsx
@@ -1,31 +1,34 @@
 import styles from "./NoteList.css";
 import type { Note } from "~/data/notes";
+import { Link } from "react-router-dom";
 
 export default function NoteList({ notes }: { notes: Note[] }) {
   return (
     <ul id="note-list">
       {notes.map((note, index) => (
         <li key={note.id} className="note">
-          <article>
-            <header>
-              <ul className="note-meta">
-                <li>#{index + 1}</li>
-                <li>
-                  <time dateTime={note.id}>
-                    {new Date(note.id).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "short",
-                      year: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </time>
-                </li>
-              </ul>
-              <h2>{note.title}</h2>
-            </header>
-            <p>{note.content}</p>
-          </article>
+          <Link to={note.id}>
+            <article>
+              <header>
+                <ul className="note-meta">
+                  <li>#{index + 1}</li>
+                  <li>
+                    <time dateTime={note.id}>
+                      {new Date(note.id).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </time>
+                  </li>
+                </ul>
+                <h2>{note.title}</h2>
+              </header>
+              <p>{note.content}</p>
+            </article>
+          </Link>
         </li>
       ))}
     </ul>
