7911cf2370b400bad4fcb9aaff50ee5eaffa6aa8
dawidlehai <haidawidle@gmail.com>
2023-01-10 12:39:24 +0100

    Fetching details about note and handling errors if there is no note with the given ID

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,18 +1,42 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+import type { Note } from "~/data/notes";
+import { getStoredNotes } from "~/data/notes";
 import styles from "~/styles/note-details.css";
 
 export default function NoteDetailsPage() {
+  const note: Note = useLoaderData();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all notes</Link>
         </nav>
-        <h1>NOTE TITLE</h1>
+        <h1>{note.title}</h1>
       </header>
-      <p id="note-details-content">NOTE CONTENT</p>
+      <p id="note-details-content">{note.content}</p>
     </main>
   );
+}
+
+export async function loader({ params }: { params: { noteId: string } }) {
+  const notes = await getStoredNotes();
+  const noteId = params.noteId;
+  const selectedNote: Note = notes.find((note: Note) => note.id === noteId);
+
+  if (!selectedNote) {
+    throw json(
+      { message: "Can't find note with the given ID." },
+      {
+        status: 404,
+        statusText: "Not found",
+      }
+    );
+  }
+
+  return selectedNote || [];
 }
 
 export function links() {
